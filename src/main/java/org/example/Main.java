23bbc28dd0bd15128063c7c5182b2270f1ec8200
Gyulguma <ybg653924@gmail.com>
2024-08-19 18:40:22 +0900

    feat: 학사일정 크롤링 기능 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,24 @@
 package org.example;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Main implements RequestHandler<Object, String> {
 	private static final String DB_URL = System.getenv("DB_URL");
 	private static final String DB_USER = System.getenv("DB_USER");
 	private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
-	private final ObjectMapper objectMapper = new ObjectMapper();
 	private LambdaLogger logger;
 
 	public Main() {
@@ -32,14 +30,9 @@ public class Main implements RequestHandler<Object, String> {
 		String result = "Success Crawling!";
 
 		try {
-			List<String> urlList = new ArrayList<>();
-			urlList.add("https://www.mju.ac.kr/mjukr/255/subview.do");
-			urlList.add("https://www.mju.ac.kr/mjukr/256/subview.do");
-			urlList.add("https://www.mju.ac.kr/mjukr/257/subview.do");
-			urlList.add("https://www.mju.ac.kr/mjukr/259/subview.do");
-			urlList.add("https://www.mju.ac.kr/mjukr/260/subview.do");
+			String url = "https://emc.mju.ac.kr/emc/5329/subview.do";
 
-			for(String url : urlList) this.notice(url);
+			this.schedule(url);
 
 		} catch (Exception var7) {
 			this.logger.log("ERROR: " + var7.getMessage());
@@ -49,13 +42,13 @@ public class Main implements RequestHandler<Object, String> {
 		return result;
 	}
 
-	private void notice(String baseUrl) throws Exception {
+	private void schedule(String baseUrl) throws Exception {
 		this.logger.log(String.format("INFO: Starting method 'notice' for baseUrl=%s\n", baseUrl));
-		String baseLink = "https://www.mju.ac.kr";
-		List<NoticeItem> allNotices = new ArrayList<>();
+
 		StringBuilder sb = new StringBuilder();
-		sb.append("insert into notice (category, notice_no, title, writer, noticedAt, views, link) values\n");
+		sb.append("insert into calendar (c_year, c_month, c_period, c_contents) values\n");
 		this.logger.log("INFO: create stringBuilder");
+
 		Document docu = null;
 		int maxRetries = 3;
 		int tryCount = 0;
@@ -78,61 +71,37 @@ public class Main implements RequestHandler<Object, String> {
 
 		this.logger.log("INFO: Successfully connect jsoup");
 		assert docu != null;
-		String element = docu.select("div._inner a._last").first().attr("href").replaceAll("\\D", "");
-		int totalPage = Integer.parseInt(element);
-		String category = docu.select("div.h1Title h1").text();
-		this.logger.log("INFO: bring category from url");
-		int latestNoticeNum = this.getLastNoticeNum(category);
+		Elements monthList = docu.select("div.scheList");
+		int year = Integer.parseInt(docu.getElementById("schdulWrap").select("div.search strong").text().replaceAll("\\D", ""));
+		this.logger.log("INFO: bring year from url");
+		int[] date = this.getLastDate();
+		int cYear = date[0];
+		int cMonth = date[1];
 
-		boolean isStop = false;
+		int count=0;
+		for(int i=1; i<=monthList.size(); i++){
+			int month = i%12==0 ? 12 : i%12;
+			if(i == 13) year++;
 
-		for(int page = 1; page <= totalPage && !isStop; ++page) {
-			if(isStop) break;
+			if(cYear > year || (cYear == year && cMonth >= month)) continue;
 
-			String url = baseUrl + "?page=" + page;
-			Document document = Jsoup.connect(url).get();
+			Elements details =  monthList.get(i-1).select("ul li dl");
 
-			Elements numbers = document.getElementsByClass("_artclTdNum");
-			Elements titles = document.getElementsByClass("_artclTdTitle");
-			Elements writers = document.getElementsByClass("_artclTdWriter");
-			Elements dates = document.getElementsByClass("_artclTdRdate");
-			Elements access = document.getElementsByClass("_artclTdAccess");
-			Elements linkViews = document.getElementsByClass("artclLinkView");
-
-			List<NoticeItem> pageNotices = new ArrayList<>();
-
-			for (int i = 0; i < numbers.size(); i++) {
-				if (!numbers.get(i).text().matches("[+-]?\\d*(\\.\\d+)?"))
-					continue;
-				if(Integer.parseInt(numbers.get(i).text()) <= latestNoticeNum) {
-					isStop = true;
-					break;
-				}
-
-				String number = numbers.get(i).text();
-				String title = titles.get(i).text().replaceAll("'", "\\\\'");;
-				String writer = writers.get(i).text();
-				String date = dates.get(i).text();
-				String accessCount = access.get(i).text();
-				String link = baseLink + linkViews.get(i).select("a").first().attr("href");
-
-				NoticeItem item = new NoticeItem(category, number, title, writer, date, accessCount, link);
-				pageNotices.add(item);
-				allNotices.add(item);
+			for(int j=0; j<details.size(); j++) {
+				sb.append("("
+					+year+","
+					+month+","
+					+"'"+details.select("dt").get(j).text()+"',"
+					+"'"+details.select("dd").get(j).text()+"'),\n");
 			}
-
-			for (NoticeItem item : pageNotices) {
-				System.out.println(item);
-				sb.append(item.toSQLString());
-			}
-
+			count++;
 		}
 
-		this.logger.log("INFO: Complete Crawling of '" + category + "'. Total notice count= " + allNotices.size());
+		this.logger.log("INFO: Complete Crawling. Total crawling month count= " + count);
 		sb.replace(sb.length() - 2, sb.length(), ";");
 		String sql = sb.toString();
 		this.logger.log("INFO: Create SQL -> "+sql);
-		if (allNotices.size() != 0) {
+		if (count != 0) {
 			Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
 			try {
@@ -172,68 +141,27 @@ public class Main implements RequestHandler<Object, String> {
 
 	}
 
-	private int getLastNoticeNum(String category) throws Exception {
-		this.logger.log(String.format("INFO: Starting method 'getLastNoticeNum' for category=%s\n", category));
-		String sql = "SELECT notice_no FROM notice WHERE category = ? ORDER BY notice_no DESC LIMIT 1;";
-		int latestNoticeNum = 0;
+	private int[] getLastDate() throws Exception {
+		this.logger.log("INFO: Starting method 'getLastDate'\n");
+		String sql = "SELECT c_year as year, c_month as month FROM calendar ORDER BY year DESC, month DESC LIMIT 1;";
+		int[] date = new int[2];
 		Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-
-			try {
-				this.logger.log("INFO: DB connection successful\n");
-				pstmt.setString(1, category);
-				ResultSet resultSet = pstmt.executeQuery();
-
-				try {
-					this.logger.log("INFO: SQL result receiving successful\n");
-					if (resultSet.next()) {
-						latestNoticeNum = Integer.parseInt(resultSet.getString("notice_no"));
-					}
-
-					this.logger.log("INFO: Category = " + category + ", Recently saved notice number = " + latestNoticeNum);
-				} catch (Throwable var12) {
-					if (resultSet != null) {
-						try {
-							resultSet.close();
-						} catch (Throwable var11) {
-							var12.addSuppressed(var11);
-						}
-					}
-
-					throw var12;
-				}
-
-				resultSet.close();
-			} catch (Throwable var13) {
-				if (pstmt != null) {
-					try {
-						pstmt.close();
-					} catch (Throwable var10) {
-						var13.addSuppressed(var10);
-					}
-				}
-
-				throw var13;
-			}
-
-			pstmt.close();
-		} catch (Throwable var14) {
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (Throwable var9) {
-					var14.addSuppressed(var9);
-				}
-			}
-
-			throw var14;
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		this.logger.log("INFO: DB connection successful\n");
+		ResultSet resultSet = pstmt.executeQuery();
+		this.logger.log("INFO: SQL result receiving successful\n");
+		if (resultSet.next()) {
+			date[0] = Integer.parseInt(resultSet.getString("year"));
+			date[1] = Integer.parseInt(resultSet.getString("month"));
 		}
+		this.logger.log(String.format("INFO: Recently saved year = %s, month = %s\n", date[0], date[1]));
 
+		resultSet.close();
+		pstmt.close();
 		conn.close();
 
-		return latestNoticeNum;
+		return date;
 	}
 
 }
